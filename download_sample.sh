30b3360d12359f861f9e1397d097e1ee5d77f20f
Shing Lyu <shing.lyu@gmail.com>
2016-01-18 16:54:58 +0800

    Added download sample script

--- download_sample.sh
+++ download_sample.sh
@@ -1,18 +1,19 @@
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <productversion id> <number of test cases>"
   echo "    <productversion id>     id of product version"
-  echo "    <number of test cases>  number of test cases you want to download, default = 20"
+  #echo "    <number of test cases>  number of test cases you want to download, default = 20"
   exit 1
 fi
 
-OUTPUT_FILE="input/full_$1.json"
 
 
-
-wget "https://moztrap.mozilla.org/api/v1/caseversion/?format=json&productversion=$1&limit=$2" -O $OUTPUT_FILE
-
-echo "$OUTPUT_FILE downloaded"
-
-
-
+MAXLIMIT=1000
+CHUNKCOUNT=11
+for (( i = 0; i < $CHUNKCOUNT; i++ )); do
+  OFFSET=$(( i * MAXLIMIT ))
+  OUTPUT_FILE="input/full_$1_$OFFSET.json"
+  #echo "https://moztrap.mozilla.org/api/v1/caseversion/?format=json&productversion=$1&offset=$OFFSET&limit=$MAXLIMIT" -O $OUTPUT_FILE
+  wget "https://moztrap.mozilla.org/api/v1/caseversion/?format=json&productversion=$1&offset=$OFFSET&limit=$MAXLIMIT" -O $OUTPUT_FILE
+  echo "$OUTPUT_FILE downloaded"
+done
 
